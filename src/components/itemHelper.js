f561d761ca7c1450628667618f14618861d8d4ab
Jaroslav Nejedlý <jardanejedly@hotmail.com>
2024-07-07 19:22:48 +0200

    resolve wrong merge
    remove sortTracks function that was there twice

--- src/components/itemHelper.js
+++ src/components/itemHelper.js
@@ -323,17 +323,6 @@ export function sortTracks (trackA, trackB) {
     return trackA.Index - trackB.Index;
 }
 
-export function sortTracks (trackA, trackB) {
-    let cmp = trackA.IsExternal - trackB.IsExternal;
-    if (cmp != 0) return cmp;
-    cmp = trackB.IsForced - trackA.IsForced;
-    if (cmp != 0) return cmp;
-    cmp = trackB.IsDefault - trackA.IsDefault;
-    if (cmp != 0) return cmp;
-
-    return trackA.Index - trackB.Index;
-}
-
 export default {
     getDisplayName: getDisplayName,
     supportsAddingToCollection: supportsAddingToCollection,
